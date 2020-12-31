975f1e57405aec76b141fd2ddf968c6d830c5334
Viva_la_vida <57423518+DongHyunKIM-Hi@users.noreply.github.com>
2020-12-31 14:46:47 +0900

    json placeholder와 api 통신

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import "./styles.css";
 
 export default function App() {
+  const [users, setUser] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const fetchApi = async () => {
+    try {
+      setUser(null);
+      setLoading(true);
+      setError(null);
+      const response = await axios.get(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+      setUser(response.data);
+    } catch (e) {
+      setError(e);
+    }
+    setLoading(false);
+  };
+  useEffect(() => fetchApi(), []);
+  if (loading) return <div>로딩중...</div>;
+  if (error) return <div>에러 발생</div>;
+  if (!users) return null;
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div>
+      {users.map((user) => (
+        <li key={user.id}>{user.name}</li>
+      ))}
     </div>
   );
 }
