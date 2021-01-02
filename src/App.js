e0e9e4b083d7fc055069f1902ed1e79bde75af65
Viva_la_vida <57423518+DongHyunKIM-Hi@users.noreply.github.com>
2021-01-02 15:50:56 +0900

    Router url 이동 실습

--- src/App.js
+++ src/App.js
@@ -1,61 +1,20 @@
 import axios from "axios";
-import React, { useEffect, useReducer } from "react";
-
-function reducer(state, action) {
-  switch (action.type) {
-    case "loading":
-      return {
-        loading: true,
-        users: null,
-        error: null
-      };
-    case "success":
-      return {
-        loading: false,
-        users: action.data,
-        error: null
-      };
-    case "error":
-      return {
-        loading: false,
-        users: null,
-        error: action.error
-      };
-    default:
-      throw new Error("뭐가 틀린겨");
-  }
-}
+import React from "react";
+import Home from "./Home";
+import { Link, Route } from "react-router-dom";
+import About from "./About";
 export default function App() {
-  const [state, dispatch] = useReducer(reducer, {
-    loading: false,
-    users: null,
-    error: null
-  });
-  const fetchApi = async () => {
-    dispatch({ type: "loading" });
-    console.log(state);
-    try {
-      const response = await axios.get(
-        "https://jsonplaceholder.typicode.com/users"
-      );
-      dispatch({ type: "success", data: response.data });
-      console.log(state);
-    } catch (e) {
-      dispatch({ tpye: "error", error: e });
-    }
-  };
-  useEffect(() => {
-    fetchApi();
-  }, []);
-  if (state.loading) return <div>로딩중...</div>;
-  if (state.error) return <div>오류 발생 ㅠㅠ</div>;
-  if (!state.users) return <div>값이 없나봐유</div>;
   return (
     <div>
-      {state.users.map((user) => (
-        <li key={user.id}>{user.name}</li>
-      ))}
-      <button onClick={() => fetchApi()}>새로 고침</button>
+      <li>
+        <Link to="/">홈으로 이동</Link>
+      </li>
+      <li>
+        <Link to="/about">about으로 이동</Link>
+      </li>
+      <hr />
+      <Route path="/" component={Home} exact />
+      <Route path="/about" component={About} />
     </div>
   );
 }
